8d88d16ba0daecc351e4506cb51ce52e96eed63e
Gabriele Santomaggio <g.santomaggio@gmail.com>
2018-03-23 22:08:18 +0100

    Wip Distribution

--- lib/distribution/node_cache.ex
+++ lib/distribution/node_cache.ex
@@ -11,18 +11,47 @@ defmodule NodeCache do
   end
 
   def init(_opts) do
+    :ets.new(:users_weight, [:set, :public, :named_table])
+    :ets.new(:messages_weight, [:set, :public, :named_table])
+    Process.flag(:trap_exit, true)
+    IO.puts "init"
     Process.send_after(self(), :update_cache, 1_000)
-    users_weight = :ets.new(:users_weight, [:set, :protected])
-    messages_weight = :ets.new(:users_weight, [:set, :protected])
-    {:ok, {users_weight, messages_weight}}
+    {:ok, %{}}
   end
 
-  def handle_info(:update_cache,  {users_weight, messages_weight}) do
-    Process.send_after(self(), :update_cache, 6_000)
-    ### update the tables chace here...
-    {:noreply, {users_weight, messages_weight}}
+  def maybe_insert({nodename, {:users_count, value}}) do
+    :true = :ets.insert(:users_weight, {nodename, :users_count, value})
   end
-  def handle_call(_msg, _from, state) do
+
+  def call() do
+    {rep, bad} = GenServer.multi_call([Node.self] ++ Node.list, NodeRepository, :get_users_weight)
+    reply_nodes(rep)
+    error_nodes(bad)
+  end
+
+  def reply_nodes(nodes) do
+    for v <- nodes, do: maybe_insert(v)
+  end
+
+  def error_nodes(nodes) do
+    {:error, nodes}
+  end
+
+  def handle_info(:update_cache, state) do
+    Process.send_after(self(), :update_cache, 5_000)
+    ### update the tables chace here...
+    spawn_link(__MODULE__, :call, [])
+    {:noreply, state}
+  end
+  def handle_info({:EXIT, _pid, _reason}, state) do
+
+    {:noreply, state}
+  end
+
+
+  def handle_call(:status, _from, state) do
+
+
     {:reply, :ok, state}
   end
 
