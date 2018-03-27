21ecc3ad8ff80ed0e42b39383a7f95f9cbe2d541
Claudio D'Alicandro <claudio@dalicandro.me>
2018-03-27 08:10:17 +0200

    Adding a session repository

---
+++ lib/easy_chat/bounded_context/session/repository.ex
@@ -0,0 +1,21 @@
+defmodule EasyChat.BoundedContext.Session.Repository do
+  @moduledoc false
+
+  def start_link do
+    Agent.start_link(&Map.new/0, name: __MODULE__)
+  end
+
+  def insert({username, pid}) do
+    Agent.update(__MODULE__, &(Map.put(&1, username, pid)))
+  end
+
+  def remove(pid) do
+    Agent.update __MODULE__, fn map ->
+      map
+      |> Enum.filter(fn {_, user_pid} -> pid != user_pid end)
+      |> Enum.into(%{})
+    end
+  end
+
+  def get_all, do: Agent.get(__MODULE__, &Map.to_list/1)
+end
---
+++ test/easy_chat/bounded_context/session/repository_test.exs
@@ -0,0 +1,26 @@
+defmodule EasyChat.BoundedContext.Session.RepositoryTest do
+  use ExUnit.Case, async: true
+
+  alias EasyChat.BoundedContext.Session.Repository, as: Subject
+
+  test "It should add a tuple rapresenting an user an his websocket pid" do
+    Subject.insert {"test", "a websocket pid"}
+
+    assert Enum.member? Subject.get_all(), {"test", "a websocket pid"}
+  end
+
+  test "If the user exists, the pid should be upgraded" do
+    Subject.insert {"test", "a websocket pid"}
+    Subject.insert {"test", "another websocket pid"}
+
+    refute Enum.member? Subject.get_all(), {"test", "a websocket pid"}
+    assert Enum.member? Subject.get_all(), {"test", "another websocket pid"}
+  end
+
+  test "It should remove an entry by pid" do
+    Subject.insert {"test", "a websocket pid"}
+    Subject.remove  "a websocket pid"
+
+    refute Enum.member? Subject.get_all(), {"test", "a websocket pid"}
+  end
+end
