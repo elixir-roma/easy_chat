b060d4b0fb5ca45b55872c50afbc99348a7d3357
Claudio D'Alicandro <claudio@dalicandro.me>
2018-04-03 10:46:43 +0200

    Protected routes

--- lib/easy_chat/bounded_context/session/router.ex
+++ lib/easy_chat/bounded_context/session/router.ex
@@ -3,23 +3,18 @@ defmodule EasyChat.BoundedContext.Session.Router do
   @moduledoc false
 
   alias EasyChat.BoundedContext.Session.PostSession
-  alias EasyChat.BoundedContext.Session.PutSession
   alias EasyChat.BoundedContext.Session.CgetSession
-  alias EasyChat.BoundedContext.Session.Guardian, as: ECGuardian
-  alias EasyChat.BoundedContext.Session.ErrorHandler, as: EH
+  alias EasyChat.BoundedContext.Session.GuardPipeline
 
   plug :match
 
-  plug Guardian.Plug.VerifyHeader
-  plug Guardian.Plug.LoadResource, allow_blank: true
-
   post "/", to: PostSession
 
-  plug Guardian.Plug.Pipeline, module: ECGuardian,
-    error_handler: EH
-  plug Guardian.Plug.EnsureAuthenticated
+  get "/"  do
+    GuardPipeline.call(conn, route: fn conn ->
+      CgetSession.call(conn, [])
+    end)
+  end
 
-  get "/", to: CgetSession
-
-  plug :dispatch
+plug :dispatch
 end
