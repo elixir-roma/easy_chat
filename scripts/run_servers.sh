97b9846af32470162de653d602b94aa315839b79
Gabriele Santomaggio <g.santomaggio@gmail.com>
2018-04-11 22:01:24 +0200

    new scripts

--- scripts/run_servers.sh
+++ scripts/run_servers.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-HTTP_PORT=8080 elixir --sname easy_chat80 -S mix run
-HTTP_PORT=8081 elixir --sname easy_chat81 -S mix run
-HTTP_PORT=8082 elixir --sname easy_chat82 -S mix run
-HTTP_PORT=8083 elixir --sname easy_chat83 -S mix run
+HTTP_PORT=8080 iex  --sname easy_chat80 -S mix
+HTTP_PORT=8081 iex  --sname easy_chat81 -S mix
+HTTP_PORT=8082 iex  --sname easy_chat82 -S mix
+HTTP_PORT=8083 iex  --sname easy_chat83 -S mix
 
 
 
